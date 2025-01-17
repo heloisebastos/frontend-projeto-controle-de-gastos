ca604b48f0ea23c6cd443f35301d629c761cbdb7
heloisebastos <heloisebastos28@hotmail.com>
2025-01-16 22:17:20 -0400

    feat: adiciona exemplo de grafico de monitoramento de finanças

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Footer from "./components/footer/Footer"
 import Home from "./pages/Home"
 import Dashboard from "./pages/Dashboard"
 import CriarDespesas from "./pages/CriarDespesas"
+import DashboardPage from "./pages/DashboardPage";
+
 import './index.css';
 
 function App() {
-  return(
+  return (
     <>
       <BrowserRouter>
         <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/criar-despesas" element={<CriarDespesas />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>  
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/criar-despesas" element={<CriarDespesas />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard-page" element={<DashboardPage />} />
+        </Routes>
       </BrowserRouter>
       <Footer />
     </>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,32 @@
-import React  from 'react';
-import {NavLink} from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom'
 import { StyledNavbar } from './styles'
 
 const Navbar = () => {
+
+    // const getShortenedName = (fullName: string | null): string => {
+    //     if (!fullName) {
+    //         return "";
+    //     }
+    //     const words = fullName.split(" ");
+    //     return words.slice(0, 1).join(" ");
+    // };
+
+    // const signWithGoogle = async () => {
+    //     const provider = new GoogleAuthProvider();
+    //     try {
+    //         await signInWithPopup(auth, provider);
+    //     } catch (error) {
+    //         console.error("Erro ao fazer login com Google", error);
+    //     }
+    // };
+
     return (
         <StyledNavbar>
             <NavLink to="/">Home</NavLink>
-            <NavLink to = "/criar-Despesas">Criar Despesa</NavLink>
-            <NavLink to = "/dashboard">Dashboard</NavLink>
+            <NavLink to="/criar-Despesas">Criar Despesa</NavLink>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+            <NavLink to="/dashboard-page">Dashboard Page</NavLink>
         </StyledNavbar>
     )
 }
