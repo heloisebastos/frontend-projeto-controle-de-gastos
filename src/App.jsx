095336d578044e8e7be4f7b18b5aabae2f59e331
heloisebastos <heloisebastos28@hotmail.com>
2025-01-20 20:33:35 -0400

    refactor: refatora `App.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./components/navbar/Navbar"
-import Footer from "./components/footer/Footer"
-import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import CriarDespesas from "./pages/CriarDespesas"
-import DashboardPage from "./pages/DashboardPage";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import CriarDespesas from "./pages//CriarDespesas";
+import Dashboard from "./pages/Dashboard";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./services/firebase";
+import "./index.css";
 
-import './index.css';
-
-function App() {
+const App = () => {
+  const [user] = useAuthState(auth);
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/criar-despesas" element={<CriarDespesas />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard-page" element={<DashboardPage />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/criar-despesas"
+          element={user ? <CriarDespesas /> : <Home />}
+        />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Home />} />
+      </Routes>
       <Footer />
-    </>
-  )
-}
-
-export default App
+    </BrowserRouter>
+  );
+};
+export default App;
