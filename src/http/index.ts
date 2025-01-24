48b44e0904b76e3f4b8b69000a27bbdb1e5ccfdb
Heloíse Bastos <100194041+heloisebastos@users.noreply.github.com>
2025-01-24 00:00:03 -0400

    feat: atualiza baseUrl no index.ts

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "https://back-aprofunda-chat-despesa.onrender.com",
+    baseURL: "https://projeto-final-pretalab.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
